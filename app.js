693e8a3e96f3e6c5411eaa580afbeb6e10c7f635
Ray Vafa <vafatabar@gmail.com>
2016-11-27 21:20:24 +1100

    setting up gulp

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ var express = require('express');
 
 var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-app.get('/', function(req,res) {
+app.get('/', function (req, res) {
     res.send('Hello World');
 });
 
-app.get('/books', function(req,res) {
-    res.send('Hello Books!');
+app.get('/books', function (req, res) {
+    res.send('Hello Books');
 });
 
-app.listen(port, function(err) {
+app.listen(port, function (err) {
     console.log('running server on port ' + port);
 });
